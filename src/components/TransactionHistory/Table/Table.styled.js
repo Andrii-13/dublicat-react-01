7e821833cb41b7ba4d5a9c4c56c96264bcb0c4c7
Andrii-13 <128071322+Andrii-13@users.noreply.github.com>
2023-10-02 00:55:48 +0300

    finish hw 1

--- src/components/TransactionHistory/Table/Table.styled.js
+++ src/components/TransactionHistory/Table/Table.styled.js
@@ -5,6 +5,7 @@ export const TableTag = styled.table`
   margin: 0 auto;
   background: white;
   margin-bottom: 100px;
+  box-shadow: 2px 2px 5px rgba(0,0,0,0.2), 2px 4px 6px rgba(0,0,0,0.2);
 `;
 
 export const TableHead = styled.thead`
